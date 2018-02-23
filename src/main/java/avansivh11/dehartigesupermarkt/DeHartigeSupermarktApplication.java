dc2d3f5d36ee4e3712d9bce77f3ab220acad8e63
Erik Koolen <erik-koolen@online.nl>
2018-02-23 17:37:20 +0100

    Initial commit

---
+++ src/main/java/avansivh11/dehartigesupermarkt/DeHartigeSupermarktApplication.java
@@ -0,0 +1,12 @@
+package avansivh11.dehartigesupermarkt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeHartigeSupermarktApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeHartigeSupermarktApplication.class, args);
+	}
+}
---
+++ src/test/java/avansivh11/dehartigesupermarkt/DeHartigeSupermarktApplicationTests.java
@@ -0,0 +1,16 @@
+package avansivh11.dehartigesupermarkt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DeHartigeSupermarktApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
