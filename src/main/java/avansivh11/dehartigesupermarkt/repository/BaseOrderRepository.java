1a1af07ce55af1f8b678c970b3a4eae0f1df82b9
Erik Koolen <erik-koolen@online.nl>
2018-03-22 10:59:34 +0100

    deleted order

--- src/main/java/avansivh11/dehartigesupermarkt/repository/BaseOrderRepository.java
+++
@@ -1,8 +0,0 @@
-package avansivh11.dehartigesupermarkt.repository;
-
-import avansivh11.dehartigesupermarkt.model.order.BaseOrder;
-import org.springframework.data.repository.CrudRepository;
-
-public interface BaseOrderRepository extends CrudRepository<BaseOrder, Long> {
-
-}
