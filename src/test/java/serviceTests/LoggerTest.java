5ccc961e1a457751f3936df9c247dd4991e25d32
DESKTOP Erik Koolen <erik-koolen@online.nl>
2018-04-02 11:48:47 +0200

    file logger test

--- src/test/java/serviceTests/LoggerTest.java
+++ src/test/java/serviceTests/LoggerTest.java
@@ -6,8 +6,13 @@ import avansivh11.dehartigesupermarkt.service.logging.ConsoleLogger;
 import avansivh11.dehartigesupermarkt.service.logging.FileLogger;
 import avansivh11.dehartigesupermarkt.service.logging.WebsiteLogger;
 import org.assertj.core.api.Assertions;
+import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class LoggerTest {
 
@@ -34,5 +39,31 @@ public class LoggerTest {
 
         //Test level 1
         Assertions.assertThat(consoleLogger.nextLogger == null);
+    }
+
+    @Test
+    public void FileLoggerTest() throws IOException {
+        //Create part of the chain
+        AbstractLogger fileLogger = new FileLogger(AbstractLogger.FILE);
+        AbstractLogger consoleLogger = new ConsoleLogger(AbstractLogger.CONSOLE);
+        fileLogger.setNextLogger(consoleLogger);
+
+        //Log the message
+        String message = "This is a file write test.";
+        int messageLength = message.length();
+        fileLogger.logMessage(AbstractLogger.FILE, message);
+
+        //Get current date for file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd").format(new Date());
+
+        //Try to read the last line of the file
+        BufferedReader input = new BufferedReader(new FileReader("src/output/Log"+ timeStamp +".txt"));
+        String line,last = "";
+        while ((line = input.readLine()) != null) {
+            last = line;
+        }
+
+        //The last line contains a timestamp. We only want to test the message itself
+        Assert.assertEquals(last.substring(0,messageLength),message);
     }
 }
